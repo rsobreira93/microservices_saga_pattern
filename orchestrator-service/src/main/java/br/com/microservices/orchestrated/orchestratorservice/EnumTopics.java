ad531f3da4ee0fd51fae710863a6aebdf3498c09
Romulo Sobreira <sobreiaromuloes@gmail.com>
2025-07-23 06:55:16 -0300

    feat: add enum to kafka topics

---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/EnumTopics.java
@@ -0,0 +1,26 @@
+package br.com.microservices.orchestrated.orchestratorservice;
+
+public enum EnumTopics {
+
+    START_SAGA("start-saga"),
+    BASE_ORCHESTRATOR("orchestrator"),
+    FINISH_SUCCESS("finish-success"),
+    FINISH_FAIL("finish-fail"),
+    PRODUCT_VALIDATION_SUCCESS("product-validation-success"),
+    PRODUCT_VALIDATION_FAIL("product-validation-fail"),
+    PAYMENT_SUCCESS("payment-success"),
+    PAYMENT_FAIL("payment-fail"),
+    INVENTORY_SUCCESS("inventory-success"),
+    INVENTORY_FAIL("inventory-fail"),
+    NOTIFY_ENDING("notify-ending");
+
+    private String topic;
+
+    public String getTopic() {
+        return topic;
+    }
+
+    EnumTopics(String topic) {
+        this.topic = topic;
+    }
+}
