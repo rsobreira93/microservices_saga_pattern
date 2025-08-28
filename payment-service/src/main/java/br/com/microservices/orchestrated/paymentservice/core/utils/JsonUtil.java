6dd6c2c9e68a0f868eae383489e2d35b7d5dd4ef
Romulo Sobreira <sobreiaromuloes@gmail.com>
2025-08-28 07:38:45 -0300

    feat: add exception and exception handler

---
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/utils/JsonUtil.java
@@ -0,0 +1,31 @@
+package br.com.microservices.orchestrated.paymentservice.core.utils;
+
+import br.com.microservices.orchestrated.paymentservice.core.dtos.Event;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JsonUtil {
+
+    private  final ObjectMapper objectMapper;
+
+    public JsonUtil(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    public String toJson(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        }catch (Exception e){
+            return "";
+        }
+    }
+
+    public Event toEvent(String json){
+        try {
+            return objectMapper.readValue(json, Event.class);
+        }catch (Exception e){
+            return null;
+        }
+    }
+}
