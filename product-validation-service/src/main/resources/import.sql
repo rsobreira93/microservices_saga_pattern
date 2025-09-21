65861e105e9ff5cddb159d56f81bfe2487db2be1
Romulo Sobreira <sobreiaromuloes@gmail.com>
2025-09-21 15:11:31 -0300

    chore: add products sql

---
+++ product-validation-service/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO products(id, code) VALUES (1, 'COMIC_BOOKS');
+INSERT INTO products(id, code) VALUES (2, 'BOOKS');
+INSERT INTO products(id, code) VALUES (3, 'MOVIES');
+INSERT INTO products(id, code) VALUES (4, 'MUSIC');
